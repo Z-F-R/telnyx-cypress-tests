f62c93a0c8c23c1c69c13a4eaae82dfbffffc3f9
ZFR <kosdraban@gmail.com>
2026-07-23 15:32:03 +0200

    added integration with Cypress Cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "hn1ccn",
   e2e: {
     baseUrl: "https://telnyx.com",
     watchForFileChanges: false,
     viewportWidth: 1440,
     viewportHeight: 900,
-    video: false,
-    screenshotOnRunFailure: false,
+    video: !!process.env.CI,
+    screenshotOnRunFailure: !!process.env.CI,
     chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       return config;
